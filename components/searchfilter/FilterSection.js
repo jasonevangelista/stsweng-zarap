8a7ca25dda0728721884537428f7d5389bb381f0
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-11-16 12:19:19 +0800

    changed filter section to Menu component

--- components/searchfilter/FilterSection.js
+++ components/searchfilter/FilterSection.js
@@ -1,12 +1,13 @@
 import styles from '../../styles/searchfilter/FilterSection.module.css'
-import {Card, Divider, Row, Col} from 'antd'
+import {Card, Divider, Menu, Button} from 'antd'
+import SubMenu from 'antd/lib/menu/SubMenu'
 
 
 export default function FilterSection({}) {
   return(
     <Card className={styles.filterSection}>
       
-      <p>Filters</p>
+      {/* <p>Filters</p>
       <p>Clear Filter</p>
 
       <div className={styles.subFilter}>
@@ -33,7 +34,36 @@ export default function FilterSection({}) {
         <p>Cuisine 2</p>
         <p>Cuisine 3</p>
         <p>See all cuisines</p>
-      </div>
+      </div> */}
+
+      <p>Filters</p>
+      <Button>
+        Clear Filter
+      </Button>
+
+      <Menu 
+        mode="inline"
+        defaultOpenKeys={['sub1', 'sub2']}
+        className={styles.filterMenu}
+      >
+        <SubMenu key="sub1" title="Sort by">
+          <Menu.Item key="1">Option 1</Menu.Item>
+          <Menu.Item key="2">Option 2</Menu.Item>
+          <Menu.Item key="3">Option 3</Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub2" title="Location">
+          <Menu.Item key="A">Option A</Menu.Item>
+          <Menu.Item key="B">Option B</Menu.Item>
+          <Menu.Item key="C">Option C</Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub3" title="Cuisines">
+          <Menu.Item key="X">Option X</Menu.Item>
+          <Menu.Item key="Y">Option Y</Menu.Item>
+          <Menu.Item key="Z">Option Z</Menu.Item>
+        </SubMenu>
+
+
+      </Menu>
 
     </Card>
   )
