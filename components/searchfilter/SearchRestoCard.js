84db5fad6d3a8d92e27f4103e06aab1d91172f67
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-11-23 09:56:45 +0800

    fixed openHours format display

--- components/searchfilter/SearchRestoCard.js
+++ components/searchfilter/SearchRestoCard.js
@@ -2,6 +2,11 @@ import styles from '../../styles/searchfilter/SearchRestoCard.module.css'
 import {Card, Divider, Rate, Row, Col, Image} from 'antd'
 
 export default function SearchRestoCard({ resto }) {
+
+  var cuisines = FormatDetails(resto.cuisineType, ", ")
+  var contactDetails = FormatDetails(resto.contactDetails, "  |  ")
+  var openHours = FormatDetails(resto.openHours, "  |  ")
+
   return(
     <Card hoverable className={styles.restoCard}>
 
@@ -32,15 +37,34 @@ export default function SearchRestoCard({ resto }) {
           <p>CONTACT NO:</p>
         </Col>
         <Col span={16}>
-          
-          <p>Food</p>
-          <p>PHP 100</p>
-          <p>7:00AM-10:00PM (Mon-Sat)</p>
-          <p>0912345</p>
+          {cuisines}
+          <p>PHP {resto.averageCost}</p>
+          {openHours}
+          {contactDetails}
         </Col>
       </Row>
       
     </Card>
   )
 
+}
+
+function FormatDetails(details, symbol){
+  var detailsString = ""
+
+  if(details.length > 1){
+    const list = []
+    for(var i = 0; i < details.length; i++){
+      detailsString += details[i]
+      if ( (i+1) < details.length)
+        detailsString += symbol
+    }
+  }
+  else{
+    detailsString = details[0]
+  }
+
+  return (
+    <p>{detailsString}</p>
+  )
 }
