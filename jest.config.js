e8337d18fe882f87522e63818c434c35997ce165
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-11-25 23:33:56 +0800

    Added enzyme and jest

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/tests/**',
+    '!**/coverage/**',
+    '!jest.config.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/mocks/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mocks/fileMock.js',
+  },
+  setupFiles: [
+    '<rootDir>/tests/setup.js'
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/tests/setupAfterEnv.js',
+  ],
+  testMatch: [
+    "**/*.(test|spec).(ts|tsx|js|jsx)"
+  ],
+  testPathIgnorePatterns: [
+    '/.next/',
+    '/node_modules/',
+    '/coverage/'
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+};
---
+++ tests/index.test.js
@@ -0,0 +1,12 @@
+import { mount } from 'enzyme';
+
+import Footer from '../components/Footer';
+
+/** @test {Footer Component} */
+describe('Footer Component', () => {
+  it('should render without crashing', () => {
+    const wrapper = mount(<Footer/>);
+    // Expect a Footer 
+    expect(wrapper.find('footer')).toHaveLength(1);
+  });
+});
