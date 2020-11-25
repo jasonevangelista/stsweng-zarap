aacd99d8c7f687b5ec899bdea9c92929a0cd6652
NeilMatthewLua <neil_lua@dlsu.edu.ph>
2020-11-25 11:03:57 +0800

    Updated cards to have strict one decimal place

--- components/carouselItem.js
+++ components/carouselItem.js
@@ -1,30 +1,49 @@
 import styles from '../styles/carouselItem.module.css';
-import { Card, Row, Col } from 'antd';  
+import { Card, Row, Col, Rate } from 'antd';  
 
-export default function carouselItem() {
+function FormatRating(rating, reviews){
+    var ratingString = "";
+    ratingString = Math.floor(rating * 2 ) / 2;
+    ratingString = ratingString.toFixed(1);
+    
+    if(reviews != null){
+        if (reviews.length > 1){
+            return (
+                <p>{ratingString} ({reviews.length} reviews)</p>
+            )
+        }
+        else{
+            return (
+                <p>{ratingString} ({reviews.length} review)</p>
+            )
+        }
+    }
+    else {
+        return (
+            <p>{ratingString} (0 review)</p>
+        )
+    }
+}
+
+export default function carouselItem({ restoSet }) {
+    
     return (  
-        <Row>
-            <Col span={6} offset={1}>
-                <Card title="Konohotdogs" bordered={false} hoverable headStyle={{color: 'rgba(255,255,255, 255)', fontSize: '2vw', fontFamily: 'Permanent Marker', border: 0 }} className={styles.card}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </Col>
-            <Col span={6} offset={2}>
-                <Card title="Konosiopao" bordered={false} hoverable headStyle={{color: 'rgba(255,255,255, 255)', fontSize: '2vw', fontFamily: 'Permanent Marker', border: 0 }} className={styles.card}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </Col>
-            <Col span={6} offset={2}>
-                <Card title="Konosiomai" bordered={false} hoverable headStyle={{color: 'rgba(255,255,255, 255)', fontSize: '2vw', fontFamily: 'Permanent Marker', border: 0 }} className={styles.card}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
-            </Col>
+        <Row justify="space-around">
+            {restoSet.map ((card, index) => {
+                return (
+                <Col span={6} key={index}>
+                    <Card title={card.name} bordered={false} hoverable headStyle={{color: 'white', fontSize: '2vw', fontFamily: 'Permanent Marker', border: 0, wordWrap: 'break-word !', whiteSpace: 'normal', height: '150px'}} className={styles.card}>
+                        <p className={styles.cardContent}>{card.establishmentType}</p>
+                        <p className={styles.cardContent}>{card.city}</p>
+                        {/* <p className={styles.cardContent}>{card.averageRating}</p> */}
+                        <div id={styles.stars}>
+                            <Rate disabled allowHalf value={card.averageRating}/>
+                            <span className="ant-rate-text">{FormatRating(card.averageRating, restoSet.reviews)}</span>
+                        </div>
+                    </Card>
+                </Col>
+                )
+            })}
         </Row>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,56 @@ import Head from "next/head";
 import { connectToDatabase } from "../util/mongodb";
 import styles from "../styles/landingpage.module.css";
 import styled from "styled-components";
-import { Input, Row, Col, Carousel, Typography } from "antd";
+import { Input, Row, Col, Carousel } from "antd";
 import CarouselItem from "../components/CarouselItem";
 
+const Search = Input;
 const RoundSearch = styled(Input.Search)`
   .ant-input {
     border-radius: 10px;
   }
 `;
 
-export default function Home({ isConnected }) {
+//Code from de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+const restoPicker = (results) => {
+  const shuffledArray = shuffle(results);
+
+  const restaurants = []
+  let tracker = 0;
+
+  while (restaurants.length < 7) {
+    if (shuffledArray[tracker].averageRating >= 3.0) {
+      restaurants.push(shuffledArray[tracker]);
+    }
+    
+    tracker++;
+  }
+  return restaurants;
+}
+
+export default function Home({ results }) {
+  let cards = restoPicker(results);
+  console.log(cards);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -26,6 +66,7 @@ export default function Home({ isConnected }) {
             <Row className={styles.searchBar}>
               <Col span={8}>
                 <RoundSearch enterButton />
+                {/* <Search enterButton /> */}
               </Col>
             </Row>
           </h2>
@@ -47,10 +88,10 @@ export default function Home({ isConnected }) {
         <div className={styles.carouselTop}>
           <Carousel autoplay dots={false}>
             <div>
-              <CarouselItem />
+              <CarouselItem restoSet={cards.slice(0,3)}/>
             </div>
             <div>
-              <CarouselItem />
+              <CarouselItem restoSet={cards.slice(3,6)}/>
             </div>
           </Carousel>
         </div>
@@ -59,12 +100,18 @@ export default function Home({ isConnected }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase();
+// get all restaurants info when page is loaded
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
 
-  const isConnected = await client.isConnected(); // Returns true or false
+  const restaurants = await db
+    .collection("restaurant")
+    .find({ })
+    .toArray();
 
   return {
-    props: { isConnected },
+    props: {
+      results: JSON.parse(JSON.stringify(restaurants)),
+    },
   };
 }
