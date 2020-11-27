09f19a00a15294d032e9329ec10c2aef3bf3b4d1
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-11-27 17:56:16 +0800

    added 404 redirector for invalid restaurant url params

--- pages/restaurant/[id].js
+++ pages/restaurant/[id].js
@@ -1,27 +1,34 @@
-import { connectToDatabase } from '../../util/mongodb'
-import { ObjectId } from 'mongodb';
+import { connectToDatabase } from "../../util/mongodb";
+import { ObjectId } from "mongodb";
 
-
-export default function SearchFilter({ resto }) {
-
-  console.log("resto:")
-  console.log(resto)
+export default function RestaurantProfile({ resto }) {
   return (
-
     <div>
       <p>Resto ID: {resto._id}</p>
       <p>Resto Name: {resto.name}</p>
     </div>
-  )
+  );
 }
 
+function isHex(str) {
+  return /^[A-F0-9]+$/i.test(str);
+}
 
 export async function getServerSideProps(context) {
-  const restaurantID = context.params.id
-  console.log('restaurant id: ' + restaurantID)
+  const restaurantID = context.params.id;
+  console.log("restaurant id: " + restaurantID);
 
-  // param must be a single string of 12 bytes or a string of 24 hex characters
-  
+  // check if params is in valid form (24-character hex string)
+  if (restaurantID.length != 24 || !isHex(restaurantID)) {
+    console.log("invalid params!!!");
+    return {
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+    };
+  }
+
   const { db } = await connectToDatabase();
 
   const restaurant = await db
@@ -30,23 +37,24 @@ export async function getServerSideProps(context) {
     .limit(1)
     .toArray();
 
-  if (restaurant.length > 0){
-    var results = JSON.parse(JSON.stringify(restaurant))
-    console.log("resto:")
-    console.log(results[0])
+  if (restaurant.length > 0) {
+    var results = JSON.parse(JSON.stringify(restaurant));
+    console.log("resto:");
+    console.log(results[0]);
     return {
       props: {
-        resto: results[0]
-      }
+        resto: results[0],
+      },
     };
   }
-  else{
-    return{
-      props: {
-        resto: null
-      }
-    }
-  }
-  
-}
 
+  // if data is empty
+  else {
+    return {
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+    };
+  }
+}
