8734171eb062826442a5faf3d067c4216a8d498f
Carlos Shi <carlos_shi@dlsu.edu.ph>
2020-11-28 20:01:45 +0800

    comment out for future feature

--- components/restoprofile/Reviews.js
+++ components/restoprofile/Reviews.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Typography, Empty, Space } from "antd";
-import Card from "./ReviewCard";
+// import Card from "./ReviewCard";
 
 const { Title } = Typography;
 
@@ -13,25 +13,26 @@ const { Title } = Typography;
 // };
 
 export default function Reviews({ reviews }) {
-  const checkReviews = (arr) => {
-    if (arr.length === 1) {
-      if (arr[0] === null) {
-        return <Empty description="There are no reviews for this resturant." />;
-      }
-    }
-  };
+  // const checkReviews = (arr) => {
+  //   if (arr.length === 1) {
+  //     if (arr[0] === null) {
+  //       return <Empty description="There are no reviews for this resturant." />;
+  //     }
+  //   }
+  // };
 
   return (
     <div>
       <Title level={3}>Reviews</Title>
+      <Empty description="There are no reviews for this resturant." />
       {/* {checkReviews(reviews)} */}
-      <Space direction="vertical" style={{ margin: "10px" }}>
+      {/* <Space direction="vertical" style={{ margin: "10px" }}>
         <Card />
         <Card />
         <Card />
         <Card />
         <Card />
-      </Space>
+      </Space> */}
     </div>
   );
 }
