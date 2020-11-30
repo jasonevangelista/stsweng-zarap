53682755402898faf633f871163c3707b241ff48
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-11-30 13:27:20 +0800

    Combined neil.test.js into index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,9 +5,12 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import CarouselItem from '../components/CarouselItem';
 import SearchRestoCard, { FormatDetails } from '../components/searchfilter/SearchRestoCard';
+import BasicInfo from '../components/restoprofile/BasicInfo';
+import Gallery from '../components/restoprofile/Gallery';
+import GalleryItem from '../components/restoprofile/GalleryItem'; 
 import ImageHeader from '../components/restoprofile/ImageHeader'; 
 import ReviewCard from '../components/restoprofile/ReviewCard'; 
-import Reviews, { countReviews, checkReviews } from '../components/restoprofile/Reviews'; 
+import Reviews, { countReviews, checkReviews } from '../components/restoprofile/Reviews';
 
 // mock window.matchMedia() method which is not implemented in JSDOM
 Object.defineProperty(window, 'matchMedia', {
@@ -130,6 +133,49 @@ describe('FormatDetails Function', () => {
         const symbol = ', ';
         const expectedResult = details[0];
         expect(FormatDetails(details, symbol)).toEqual(<p>{expectedResult}</p>);
+    });
+});
+
+/** @test {BasicInfo Component} */
+describe('BasicInfo Component', () => {
+    it('should render without crashing', () => {
+        const testProps = {
+            name: 'Golden Fortune',
+            establishmentType: 'Casual Dining',
+            city: 'Manila',
+            fullAddress: '678 T.M. Kalaw Avenue, Ermita, Manila',
+            cuisineType: ['Seafood', 'Chinese'],
+            averageCost: 800,
+            openHours: ['11:00 AM-2:30 PM (Mon-Sat)', '5:30 PM-12:AM (Sun)'],
+            contactDetails: ['02 85222288', '02 85222288'],
+            averageRating: 3.9,
+            reviews: []
+        };
+
+        const wrapper = mount(<BasicInfo resto={testProps}/>);
+        // Expect a Basic Info Component
+        expect(wrapper.find(BasicInfo)).toHaveLength(1);
+        wrapper.unmount();
+    });
+});
+
+/** @test {Gallery Component} */
+describe('Gallery Component', () => {
+    it('should render without crashing', () => {
+        const wrapper = mount(<Gallery />);
+        // Expect a Header
+        expect(wrapper.find(Gallery)).toHaveLength(1);
+        wrapper.unmount();
+    });
+});
+
+/** @test {GalleryItem Component} */
+describe('GalleryItem Component', () => {
+    it('should render without crashing', () => {
+        const wrapper = mount(<Gallery />);
+        // Expect a Header
+        expect(wrapper.find(GalleryItem)).toHaveLength(7);
+        wrapper.unmount();
     });
 });
 
