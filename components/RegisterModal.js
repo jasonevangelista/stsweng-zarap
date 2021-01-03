5942a01e2bad8b11e2e68d77645b4e51bf1ec653
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2021-01-03 15:43:07 +0800

    added success prompt for registration

--- components/RegisterModal.js
+++ components/RegisterModal.js
@@ -1,13 +1,13 @@
 /*eslint no-undef: 0*/
 import styles from '../styles/registerModal.module.css';
 import { Input, Button, Form, Modal, Tooltip } from 'antd';
+import { CheckCircleTwoTone } from '@ant-design/icons';
 import { useState } from 'react';
-
-import bcrypt from 'bcryptjs';
 
 export default function RegisterModal(props) {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
+  const [registered, setRegistered] = useState(false);
 
   // called when form content is valid
   const onFinish = (values) => {
@@ -16,11 +16,24 @@ export default function RegisterModal(props) {
     console.log('Received values of form:'); // values.firstName | values.lastName | values.email | values.password
     console.log(values)
     addAccount(values).then(()=>{
-      props.onFinish();
       setLoading(false);
-      form.resetFields();
+      setRegistered(true);
     })
   };
+
+  const onClose = () =>{
+    form.resetFields();
+    setLoading(false);
+    if(registered){
+      props.onFinish();
+      setTimeout(() => {
+        setRegistered(false);
+      },500)
+    }
+    else{
+      props.closeModal();
+    }
+  }
 
   function isAlphaNumeric(str) {
     return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(str);
@@ -31,152 +44,166 @@ export default function RegisterModal(props) {
       centered
       visible={props.registerModalVisible}
       onCancel={() => {
-        form.resetFields();
-        props.closeModal();
+        onClose()
       }}
       width={400}
       footer={null}>
-      <Form
-        form={form}
-        onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
-        className={styles.form}
-        // {...layout}
-      >
-        <h1>Register</h1>
 
-        <Form.Item
-          name="firstName"
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: 'Please input your first name!',
-              whitespace: true
-            }
-          ]}>
-          <Input placeholder="first name" style={{ borderRadius: '7px' }} />
-        </Form.Item>
+      {registered &&
+        <div className={styles.registeredMessage}>
+          <CheckCircleTwoTone style={{ fontSize: '50px'}} twoToneColor="#52c41a"/>
+          <h1>Success!</h1>
+          <p>Your account has been registered!</p>
+          <Button type="primary" className={styles.btnSuccess} onClick={()=>{
+            onClose();
+          }}>OK</Button>
+        </div>
+      }
 
-        <Form.Item
-          name="lastName"
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: 'Please input your last name!',
-              whitespace: true
-            }
-          ]}>
-          <Input id="lastName" placeholder="last name" style={{ borderRadius: '7px' }} />
-        </Form.Item>
+      {!registered &&
+        <Form
+          form={form}
+          onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          className={styles.form}
+          // {...layout}
+        >
+          
+          <h1>Register</h1>
 
-        <Form.Item
-          name="email"
-          hasFeedback
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not a valid e-mail!'
-            },
-            {
-              required: true,
-              message: 'Please input your e-mail!'
-            },
-            {
-              validator: async (rule, value) => {
-                if (value) {
-                  var emailStatus = await checkEmailDuplicate(value);
-                  return emailStatus;
-                }
-                return Promise.resolve();
-              }
-            }
-          ]}>
-          <Input id="email" placeholder="e-mail" style={{ borderRadius: '7px' }} />
-        </Form.Item>
-
-        <Tooltip
-          title="Password must be at least 6 characters and must only be alphanumeric!"
-          placement="top"
-          name="password"
-          trigger={["focus"]}>
           <Form.Item
-            name="password"
+            name="firstName"
             hasFeedback
             rules={[
               {
                 required: true,
-                message: 'Please input your password!'
+                message: 'Please input your first name!',
+                whitespace: true
+              }
+            ]}>
+            <Input placeholder="first name" style={{ borderRadius: '7px' }} />
+          </Form.Item>
+
+          <Form.Item
+            name="lastName"
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please input your last name!',
+                whitespace: true
+              }
+            ]}>
+            <Input id="lastName" placeholder="last name" style={{ borderRadius: '7px' }} />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            hasFeedback
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not a valid e-mail!'
               },
-              () => ({
-                validator(rule, value) {
-                  if (!value || value.length >= 6) {
-                    if (value && !isAlphaNumeric(value)) {
-                      return Promise.reject('Password must be alphanumeric!');
-                    }
-                    return Promise.resolve();
-                  } else if (value.length > 0) {
-                    return Promise.reject('Password is too short!');
+              {
+                required: true,
+                message: 'Please input your e-mail!'
+              },
+              {
+                validator: async (rule, value) => {
+                  if (value) {
+                    var emailStatus = await checkEmailDuplicate(value);
+                    return emailStatus;
                   }
                   return Promise.resolve();
                 }
+              }
+            ]}>
+            <Input id="email" placeholder="e-mail" style={{ borderRadius: '7px' }} />
+          </Form.Item>
+
+          <Tooltip
+            title="Password must be at least 6 characters and must only be alphanumeric!"
+            placement="top"
+            name="password"
+            trigger={["focus"]}>
+            <Form.Item
+              name="password"
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!'
+                },
+                () => ({
+                  validator(rule, value) {
+                    if (!value || value.length >= 6) {
+                      if (value && !isAlphaNumeric(value)) {
+                        return Promise.reject('Password must be alphanumeric!');
+                      }
+                      return Promise.resolve();
+                    } else if (value.length > 0) {
+                      return Promise.reject('Password is too short!');
+                    }
+                    return Promise.resolve();
+                  }
+                })
+              ]}>
+              <Input.Password id="password" placeholder="password" style={{ borderRadius: '7px' }} />
+            </Form.Item>
+          </Tooltip>
+          <Form.Item
+            name="confirm"
+            hasFeedback
+            dependencies={['password']}
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!'
+              },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+
+                  return Promise.reject('The two passwords that you entered do not match!');
+                }
               })
             ]}>
-            <Input.Password id="password" placeholder="password" style={{ borderRadius: '7px' }} />
+            <Input.Password
+              id="confirm"
+              placeholder="confirm password"
+              style={{ borderRadius: '7px' }}
+            />
           </Form.Item>
-        </Tooltip>
-        <Form.Item
-          name="confirm"
-          hasFeedback
-          dependencies={['password']}
-          rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!'
-            },
-            ({ getFieldValue }) => ({
-              validator(rule, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
 
-                return Promise.reject('The two passwords that you entered do not match!');
-              }
-            })
-          ]}>
-          <Input.Password
-            id="confirm"
-            placeholder="confirm password"
-            style={{ borderRadius: '7px' }}
-          />
-        </Form.Item>
-
-        {/* <Form.Item > */}
-        <div className={styles.submitFormItem}>
-          <Button
-            type="primary"
-            htmlType="submit"
-            id="btnSubmit"
-            className={styles.btnSubmit}
-            loading={loading}>
-            SIGN UP
-          </Button>
-        </div>
-        {/* </Form.Item> */}
-        <div className={styles.registerFooter}>
-          Already a member?{' '}
-          <Button
-            type="text"
-            className={styles.btnRedirect}
-            onClick={() => {
-              form.resetFields();
-              props.redirectToLoginModal();
-            }}>
-            Sign in
-          </Button>
-        </div>
-      </Form>
+          {/* <Form.Item > */}
+          <div className={styles.submitFormItem}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              id="btnSubmit"
+              className={styles.btnSubmit}
+              loading={loading}>
+              SIGN UP
+            </Button>
+          </div>
+          {/* </Form.Item> */}
+          <div className={styles.registerFooter}>
+            Already a member?{' '}
+            <Button
+              type="text"
+              className={styles.btnRedirect}
+              onClick={() => {
+                form.resetFields();
+                props.redirectToLoginModal();
+              }}>
+              Sign in
+            </Button>
+          </div>
+        </Form>
+    }
     </Modal>
   );
 }
@@ -201,10 +228,9 @@ async function checkEmailDuplicate(emailInput) {
 }
 
 async function addAccount(newAccount){
-  // encrypt password
-  newAccount.password = bcrypt.hashSync(newAccount.password, 10)
   // insert new account info in db
   var accountString = JSON.stringify(newAccount);
+
   const res = await fetch('/api/register/' + accountString);
   const results = await res.json();
 
