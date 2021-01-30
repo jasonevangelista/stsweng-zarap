51262b1bd9ad78dbdb48f39712d7bdfb79cdf24d
Carlos Shi <carlos_shi@dlsu.edu.ph>
2021-01-30 22:01:41 +0800

    removed unused variables

--- components/userprofile/UserReviews.js
+++ components/userprofile/UserReviews.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Typography, Empty, Space, Pagination } from 'antd';
-import Card from "./UserReviewCard";
+import { Empty, Space, Pagination } from 'antd';
+import Card from './UserReviewCard';
 import styles from '../../styles/userreview.module.css';
 
 export default function UserReviews({ reviews }) {
@@ -13,28 +13,27 @@ export default function UserReviews({ reviews }) {
       {hasReviews(reviews) && (
         <div>
           {reviewCards[cardSetIndex]}
-          <Space direction="vertical" style={{ margin: "10px" }}>
-          </Space>
+          <Space direction="vertical" style={{ margin: '10px' }}></Space>
           <div className={styles.paginationDiv}>
-            <Pagination 
-              className={styles.userReviewPagination} 
+            <Pagination
+              className={styles.userReviewPagination}
               onChange={(page) => {
-                setCardSetIndex(page-1)
-              }} 
-              defaultCurrent={1} 
-              pageSize={pageSize} 
+                setCardSetIndex(page - 1);
+              }}
+              defaultCurrent={1}
+              pageSize={pageSize}
               total={reviews.length}
-              showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} reviews`} />
-         </div>
+              showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} reviews`}
+            />
+          </div>
         </div>
       )}
 
       {!hasReviews(reviews) && (
         <Empty description="You currently do not have reviews for any restaurant." />
       )}
-
     </div>
-  )
+  );
 }
 
 const generateUserReviewCards = (reviews, pageSize) => {
@@ -43,9 +42,9 @@ const generateUserReviewCards = (reviews, pageSize) => {
   for (const [index, value] of reviews.entries()) {
     cards.push(<Card key={index} review={value}></Card>);
   }
-  while(cards.length) groupedCards.push(cards.splice(0, pageSize));
+  while (cards.length) groupedCards.push(cards.splice(0, pageSize));
   return groupedCards;
-}
+};
 
 export const countReviews = (reviews) => {
   let count = 0;
@@ -64,8 +63,8 @@ export const checkReviews = (arr) => {
 };
 
 export const hasReviews = (arr) => {
-  if(arr){
-    if(arr.length > 0){
+  if (arr) {
+    if (arr.length > 0) {
       return true;
     }
   }
